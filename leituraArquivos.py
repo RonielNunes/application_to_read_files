2d947e64b4eca49285b89be7143ccb07e810d65c
Roniel Nunes <roniel.barbosa@ufv.br>
2021-06-07 13:09:39 -0300

    new version of the function file read

--- leituraArquivos.py
+++ leituraArquivos.py
@@ -1,12 +1,27 @@
 import os.path
+from typing import Text
  
-def leituraArquivo(file_directory):
+def leituraArquivo(file_directory) -> str:
+    text = '' # empity text - texto vazio
     if os.path.isfile(file_directory): #Verifica se o arquovo é uma file retornando true or false
         with open(file_directory) as file: # leitura do abertura do arquivo
-            print(file.read()) #leiura das linhas que compõem o texto
+            for line in file: 
+                #print(line) #leiura das linhas que compõem o texto
+                text += line #armazenando
+            #print('Text: ' + text)
+        file.close()
     else:
-        print('Error in directory or archives no existes')
+         text = 'Error in directory or archives no existes'
+    return text
+    
+
+
 
 file_directory = './ArquivosPdfs/teste.pdf'
 
-leituraArquivo(file_directory=file_directory) 
+texto = leituraArquivo(file_directory=file_directory) 
+if texto != '':
+    ##pass
+    print(texto)
+else:
+    print('Error')
