d63716d45c109c9211c4e015f6d2604ee7d78c68
Roniel Nunes <roniel.barbosa@ufv.br>
2021-06-07 12:19:45 -0300

    criação do arquivo main e desenvolvimento lógico do projeto idealizado

---
+++ main.py
@@ -0,0 +1,11 @@
+# Way dev program
+    #Organização do projeto
+    #criar makefile
+    #desenvolver
+#ler o diretorio de um ontre se encontra o arquivo 
+
+# copiar o texto para uma variavel
+
+# interpretar a leitura com o pyttsx3
+    #criar um função a parte
+    # ver se tem como fazer alguma interface bonita ou algo do tipo
